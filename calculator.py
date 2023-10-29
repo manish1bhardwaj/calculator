e9901f72523e471745151df3bc459cb4163d307f
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-10-29 22:25:26 +0530

    basic calculator

---
+++ calculator.py
@@ -0,0 +1,18 @@
+import calculator
+a=float(input())
+b=float(input())
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+power=a**b
+modulus=a%b
+floordiv=a//b
+print('sum is',add)
+print('subtraction is',sub)
+print('multiply is',mul)
+print('divide is',div)
+print('remainder is',modulus)
+print('quotient',floordiv)
+print('exponent is',power)
+
